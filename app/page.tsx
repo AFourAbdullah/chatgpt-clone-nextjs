13a8b043266a081bffaf9417412587a8ea2d3886
Abdullah Aziz <abdullahazees4@gmail.com>
2023-09-14 20:17:51 +0500

    files added

--- app/page.tsx
+++ app/page.tsx
@@ -11,23 +11,23 @@ export default function Home() {
       <div className="bg-white rounded-lg p-4 shadow-lg max-w-xl w-full">
         <div className="text-gray-800">Output</div>
       </div>
-      <div className="flex w-full h-auto">
-        <div className="relative w-full ">
+      <div className="flex w-[90%] sm:w-[80%] lg:w-[50%] h-auto absolute bottom-9 z-20 py-2  items-center">
+        <div className="relative w-full  ">
           <textarea
-            rows={query.length > 200 ? 30 : 2}
-            className="m-0 w-full border border-black/10 focus:outline-none bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-xl shadow-xs dark:shadow-xs p-3"
+            rows={query.length > 200 ? 9 : 1}
+            className="m-0  h-full  w-full border-2 border-black/1- focus:outline-none bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-xl shadow-2xl dark:shadow-xs p-3"
             placeholder="Search..."
             value={query}
             onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
               setQuery(e.target.value)
             }
+            style={{ resize: "none" }}
             // style={{ height: query.length > 300 ? "10rem" : "4rem" }} // Dynamically adjust the height based on content
           />
-          {query && (
-            <button className="absolute right-4 bottom-3 text-gray-500 rounded p-2">
-              <BiSolidRightArrow />
-            </button>
-          )}
+
+          <button className="absolute bottom-4 flex items-center text-lg md:text-2xl right-4 md:bottom-3 text-gray-500 rounded p-2">
+            <BiSolidRightArrow />
+          </button>
         </div>
       </div>
     </main>
